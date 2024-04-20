030724482eaa0362625d48dd36fb93a082d2847e
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-20 14:51:52 +0200

    Feat: declare variables and functions
    Declared variables and empty functions in javascript.js
    Also used comments to increase code readability

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+// VARIABLES
+
+// Form variables
+const inputField = document.querySelector("#amount");
+const submitButton = document.querySelector("#submit");
+
+// Pixel for the Sketch
 const pixel = document.createElement("div");
 pixel.style.height("16px");
 pixel.style.width("16px");
+
+// FUNCTIONS
+function createGrid(amount) {
+  if (inputField.value > 0 && inputField.value < 100) {
+  }
+}
+
+// EVENT HANDLERS
