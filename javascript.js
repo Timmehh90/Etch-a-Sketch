7ade20289d17a109713250f221022c24a4813897
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-20 15:07:49 +0200

    Feat: add createGrid function to make the canvas
    Implemented a createGrid function which will check if the input field has a number between 1-100.
    If so it will append a child to sketchContainer
    
    Moved pixel css from javascript to css file

--- javascript.js
+++ javascript.js
@@ -4,13 +4,24 @@
 const inputField = document.querySelector("#amount");
 const submitButton = document.querySelector("#submit");
 
+// Sketch container
+const sketchContainer = document.querySelector(".sketch-container");
+
 // Pixel for the Sketch
 const pixel = document.createElement("div");
-pixel.style.height("16px");
-pixel.style.width("16px");
+pixel.classList.add("pixel");
 
 // FUNCTIONS
-function checkInput() {}
-function createGrid(amount) {}
+function createGrid(input) {
+  if (input > 0 && input < 100) {
+    for (let i = 0; i < input; i++) {
+      sketchContainer.appendChild(pixel);
+    }
+  }
+}
 
 // EVENT HANDLERS
+
+submitButton.addEventListener("click", () => {
+  createGrid(inputField.value);
+});
