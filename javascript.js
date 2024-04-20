2a73b3be276e1ba22cb80122cc9c627eb1ae7cd6
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-20 14:54:45 +0200

    Refractor: edit function to actually be empty
    Also add new empty function: checkInput

--- javascript.js
+++ javascript.js
@@ -10,9 +10,7 @@ pixel.style.height("16px");
 pixel.style.width("16px");
 
 // FUNCTIONS
-function createGrid(amount) {
-  if (inputField.value > 0 && inputField.value < 100) {
-  }
-}
+function checkInput() {}
+function createGrid(amount) {}
 
 // EVENT HANDLERS
