c465aa64d36a0f59346fef83900582f6257bdaf4
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-21 11:27:18 +0200

    Style: add hover effect to pixel
    Added a transition on mouse hover, as well as on the original .pixel to leave a trail.
    Refractor max pixels of canvas to 500 from 100

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ const sketchContainer = document.querySelector(".sketch-container");
 
 // FUNCTIONS
 function createGrid(input) {
-  if (input > 0 && input < 100) {
+  if (input > 0 && input <= 500) {
     for (let i = 0; i < input; i++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
